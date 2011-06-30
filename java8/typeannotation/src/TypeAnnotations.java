1eb0c95e501a6ffc2aa89f594a429984ad4deccd
Nico Steppat <nico.steppat@caelum.com.br>
2011-06-29 21:55:31 -0300

    minor

--- java8/typeannotation/src/TypeAnnotations.java
+++ java8/typeannotation/src/TypeAnnotations.java
@@ -11,23 +11,17 @@ import java.util.*;
 public class TypeAnnotations {
 
 	public static void main(String ... args) {
-		System.out.println(example());
-	}
 
- 	public static @NonNull String example() {
-
-	    @NonNull String valor = null; //compile error
+	    @NonNull String valor = "null"; //compile error
 	    @NonNull String outro = "Tambem nao nulo";
 
 	    List<@NonNull ?> g; //ok
 	    List<@NonNull String> lista = new LinkedList<@NonNull String>();
 	    lista.add(valor);
 	    lista.add(outro);
-	    lista.add(null); //compile error
+	    lista.add("null"); //compile error
 
 	    @NonNull String segundo = lista.get(1);
-
-	    return segundo;
   	}
 }
 
